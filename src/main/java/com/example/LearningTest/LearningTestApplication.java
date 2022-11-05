99c658d46f589eafda6da514e563657681c7204b
nguyen cuong <>
2022-11-05 13:28:13 +0700

    init

---
+++ src/main/java/com/example/LearningTest/LearningTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearningTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LearningTest/LearningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearningTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
